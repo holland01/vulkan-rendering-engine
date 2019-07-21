801f80f5d943da9071e5bf0700c61eea556c0e7d
Holland Schutte <hgschutte1@gmail.com>
2019-07-21 14:45:12 -0700

    working triangle

--- base/main.cpp
+++ base/main.cpp
@@ -6,9 +6,83 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 
-using namespace glm;
+#include "render.h"
 
-#define GLSL(src) #src
+static const char* g_shader_vertex = GLSL(layout(location = 0) in vec3 in_Position;
+                                          layout(location = 1) in vec4 in_Color;
+
+                                          smooth out vec4 frag_Color;
+
+                                          void main() {
+                                            gl_Position = vec4(in_Position, 1.0);
+                                            frag_Color = in_Color;
+                                          });
+
+static const char* g_shader_fragment = GLSL(smooth in vec4 frag_Color;
+                                            out vec4 fb_Color;
+
+                                            void main() {
+                                              fb_Color = frag_Color;
+                                            });
+struct {
+  GLuint vao;
+  GLuint vbo;
+  GLuint program;
+} static g_api_data = {
+  0
+};
+
+struct gl_color_vertex {
+  glm::vec3 position;
+  glm::vec4 color;
+};
+
+static void init_api_data() {
+  float s = 0.5f;
+  float t = -0.5f;
+  
+  float vertices[] = {
+    0.0f, 0.0f, t, 1.0f, 0.0f, 0.0f, 1.0f,
+    s, 0.0f, t, 1.0f, 1.0f, 1.0f, 1.0f,
+    s, s, t, 0.0f, 0.0f, 1.0f, 1.0f
+  };
+  
+  GL_FN(glGenBuffers(1, &g_api_data.vbo));
+  GL_FN(glGenVertexArrays(1, &g_api_data.vao));
+  
+  GL_FN(glBindVertexArray(g_api_data.vao));
+  GL_FN(glBindBuffer(GL_ARRAY_BUFFER, g_api_data.vbo));
+
+  GL_FN(glEnableVertexAttribArray(0));
+  GL_FN(glVertexAttribPointer(0,
+                              3,
+                              GL_FLOAT,
+                              GL_FALSE,
+                              sizeof(float) * 7,
+                              (void*) 0));
+
+  GL_FN(glEnableVertexAttribArray(1));
+  GL_FN(glVertexAttribPointer(1,
+                              4,
+                              GL_FLOAT,
+                              GL_FALSE,
+                              sizeof(float) * 7,
+                              (void*)(sizeof(float) * 3)));
+
+  GL_FN(glBufferData(GL_ARRAY_BUFFER,
+                     sizeof(vertices),
+                     &vertices[0],
+                     GL_STATIC_DRAW));
+
+  g_api_data.program = make_program(g_shader_vertex, g_shader_fragment);
+
+  GL_FN(glViewport(0, 0, 640, 480));
+  
+  GL_FN(glDisable(GL_CULL_FACE));
+  GL_FN(glEnable(GL_DEPTH_TEST));
+  GL_FN(glDepthFunc(GL_LEQUAL));
+  GL_FN(glClearDepth(1.0f));
+}
 
 static void error_callback(int error, const char* description) {
   fputs(description, stdout);
@@ -21,6 +95,10 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 
 static void render(GLFWwindow* window) {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  GL_FN(glUseProgram(g_api_data.program));
+  GL_FN(glDrawArrays(GL_TRIANGLES, 0, 3));
+  GL_FN(glUseProgram(0));
 }
 
 int main(void) {
@@ -31,8 +109,8 @@ int main(void) {
   if (!glfwInit())
     exit(EXIT_FAILURE);
 
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_FALSE);
   glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
@@ -56,8 +134,10 @@ int main(void) {
 
   glfwSetKeyCallback(window, key_callback);
 
-  glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
+  init_api_data();
+  
   while (!glfwWindowShouldClose(window)) {
     render(window);
 
