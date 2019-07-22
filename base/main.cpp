d331c74d26b460966587ec616ef6fdaf508cead6
Holland Schutte <hgschutte1@gmail.com>
2019-07-21 19:31:51 -0700

    basic camera calculations

--- base/main.cpp
+++ base/main.cpp
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 #include <GL/glew.h>
 
 #include <GLFW/glfw3.h>
+
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 #include "render.h"
 
@@ -32,9 +35,149 @@ struct {
   0
 };
 
-struct gl_color_vertex {
-  glm::vec3 position;
-  glm::vec4 color;
+// key facts about transformations:
+// - a matrix transform represents the source space from the perspective
+// of the destination space
+
+struct view_data {
+  glm::mat4 proj;    // proj: camera to clip space transform
+
+  glm::mat3 orient;  // orient: camera orientation
+  
+  glm::vec3 position;  // position: theoretical position of the camera: technically,
+                       // view space is always centered at a specific location that's
+                       // k units of distance before the near plane. Thus, it's always
+                       // looking down -z. In other words, the viewing volume (frustum)
+                       // is always in the same space, and the camera facing its near plane is also
+                       // always at the same space. The "position" vector we have here
+                       // is used to provide the illusion of a moving camera. More on this later.
+
+  float step;          // step: move step amount
+
+  uint16_t view_width;
+  uint16_t view_height;
+  
+  view_data(uint16_t width, uint16_t height)
+    : proj(1.0f),
+      orient(1.0f),
+      position(0.0f),
+      step(0.1f),
+      view_width(width),
+      view_height(height)
+  {}
+
+  // Pick a translation direction, and this will modify it
+  // so that any object's within the viewing volume will be oriented/moved
+  // to provide the illusion that the viewer is moving in the requested direction.
+#define RET_VIEW_DIR(x, y, z) glm::vec3 dir(glm::inverse(orient) * glm::vec3(x, y, z)); return dir
+  
+  glm::vec3 view_up() const { RET_VIEW_DIR(0.0f, 1.0f, 0.0f); }
+  glm::vec3 view_down() const { RET_VIEW_DIR(0.0f, -1.0f, 0.0f); }
+
+  glm::vec3 view_right() const { RET_VIEW_DIR(1.0f, 0.0f, 0.0f); }
+  glm::vec3 view_left() const { RET_VIEW_DIR(-1.0f, 0.0f, 0.0f); }
+  
+  glm::vec3 view_front() const { RET_VIEW_DIR(0.0f, 0.0f, -1.0f); }
+  glm::vec3 view_back() const { RET_VIEW_DIR(0.0f, 0.0f, 1.0f); }
+
+#undef RET_VIEW_DIR
+
+  //
+  // view(): compute the model to camera transform.
+  //
+  // We need two proper transforms for this:
+  // the first is our translation, the second is 
+  // the orientation for the view.
+  // Let T be the translation matrix and O
+  // be our orientation.
+  // 
+  // Back to the idea of the "position",
+  // mentioned earlier: we're performing an illusion
+  // in terms of how we express the camera's movement
+  // relative to the world. In other words, the camera _isn't_ moving,
+  // but the world around it is.
+  //
+  // Suppose we have some model with a corresponding transform M
+  // that we wish to render. We know that M is going to have its own
+  // separate orientation and position offset within our world. 
+  // Thus, the translation we're creating here is designed to move
+  // the model represented by M a series of units _closer_ to the viewing volume.
+  //
+  // That series of units is our camera's "position".
+  //
+  // Suppose p is our position vector, then T equals:
+  //
+  // T = | 1 0 0 -p.x |
+  //     | 0 1 0 -p.y |
+  //     | 0 0 1 -p.z |
+  //     | 0 0 0   1  |
+  //
+  // Now, suppose we don't care about orientation for the moment.
+  // Ignoring the camera to clip transform as well,
+  // when the model M transform is used with T, we can express the resulting
+  // transform Q as:
+  //
+  // Q = TM
+  //
+  // If our resulting transform Q contains a translation whose coordinates
+  // lie _outside_ of the viewing volume, then the object will be clipped,
+  // and we can think of the viewer simply not having moved far enough to spot
+  // the object.
+  //
+  // However, the further they "move" in the direction of the object, the more the
+  // object itself will actually move toward the viewing volume.
+  // 
+  // Now, as far as the orientation, what we want to do is maintain that same principle,
+  // but ensure that objects in our viewing volume maintain their distance irrespective
+  // of the orientation of the viewer.
+  //
+  // A mouse move to the left is supposed to produce a clockwise rotation
+  // about the vertical axis of any object in the viewing volume that we wish to render.
+  //
+  // We don't want the vertices of an object to be rotated about the object's center of origin though;
+  // rather, we want to them to be rotated about the center of the origin of the _viewer_.
+  // Thus, we perform the translation _first_, and then orientation second.
+  //
+  glm::mat4 view() const {
+    return glm::mat4(orient) * glm::translate(glm::mat4(1.0f),
+                                              -position); 
+  }
+
+  //
+  // operator ():
+  //
+  // move struct is volatile because we're using bitsets here.
+  //
+  // very simple: if the corresponding direction is set to 1, 
+  // we compute the appropriate offset for that direction and add its
+  // scaled value to our position vector.
+  //
+  volatile struct move {
+    uint8_t up : 1;
+    uint8_t down : 1;
+    uint8_t right : 1;
+    uint8_t left : 1;
+    uint8_t front : 1;
+    uint8_t back : 1;
+    uint8_t __rem : 2;
+  };
+
+
+#define TESTDIR(move, dir)                        \
+  if ((move).dir == true) {                       \
+    this->position += view_##dir() * this->step;  \
+  }
+  
+  void operator ()(move m) {
+    TESTDIR(m, up);
+    TESTDIR(m, down);
+    TESTDIR(m, right);
+    TESTDIR(m, left);
+    TESTDIR(m, front);
+    TESTDIR(m, back);
+  }
+  
+#undef TESTDIR
 };
 
 static void init_api_data() {
