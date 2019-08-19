e7f3a10e23a848db2c05b03a438a1e7f09d6dbe1
Holland Schutte <hgschutte1@gmail.com>
2019-08-19 01:18:04 -0700

    for now define base mip level formats to 1 (mipmaps will be added later)

--- base/textures.h
+++ base/textures.h
@@ -51,19 +51,24 @@ struct textures: public type_module {
         switch (type) {
         case GL_TEXTURE_CUBE_MAP:
             texparams.insert(texparams.end(), {
-                GL_TEXTURE_MIN_FILTER, GL_LINEAR,
-                GL_TEXTURE_MAG_FILTER, GL_LINEAR,
-                GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE,
-                GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE,
-                GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE
+                    GL_TEXTURE_MIN_FILTER, GL_LINEAR,
+                        GL_TEXTURE_MAG_FILTER, GL_LINEAR,
+                        GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE,
+                        GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE,
+                        GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE,
+                        GL_TEXTURE_BASE_LEVEL, 0,
+                        GL_TEXTURE_MAX_LEVEL,0
               });
             break;
         case GL_TEXTURE_2D:
             texparams.insert(texparams.end(), {
-                GL_TEXTURE_MIN_FILTER, GL_LINEAR,
-                GL_TEXTURE_MAG_FILTER, GL_LINEAR,
-                GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE,
-                GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE
+                    GL_TEXTURE_MIN_FILTER, GL_LINEAR,
+                    GL_TEXTURE_MAG_FILTER, GL_LINEAR,
+                    GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE,
+                    GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE,
+                    GL_TEXTURE_BASE_LEVEL, 0,
+                    GL_TEXTURE_MAX_LEVEL,0
+
                   });
             break;
         }
