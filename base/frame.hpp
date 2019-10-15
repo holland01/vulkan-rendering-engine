88abdc62764b90937ca517c5ee08fae609544a0c
schutth <schutth@wwu.edu>
2019-10-15 04:15:35 -0700

    whitespace/indendation

--- base/frame.hpp
+++ base/frame.hpp
@@ -8,86 +8,86 @@
 #include <glm/gtc/matrix_transform.hpp>
 
 struct framebuffer_ops {
-    using index_type = int32_t;
-    
-    uint32_t count;
-    uint32_t width;
-    uint32_t height;
+  using index_type = int32_t;
+  
+  uint32_t count;
+  uint32_t width;
+  uint32_t height;
 
-    using face_mats_type = std::array<mat4_t, 6>;
+  using face_mats_type = std::array<mat4_t, 6>;
 
-    static const inline index_type k_uninit = -1;
-    
-    struct fbo2d {
-      darray<GLuint> fbos;
-      darray<uint32_t> widths;
-      darray<uint32_t> heights;
-      darray<module_textures::index_type> color_attachments;
-      darray<module_textures::index_type> depth_attachments;
+  static const inline index_type k_uninit = -1;
+  
+  struct fbo2d {
+    darray<GLuint> fbos;
+    darray<uint32_t> widths;
+    darray<uint32_t> heights;
+    darray<module_textures::index_type> color_attachments;
+    darray<module_textures::index_type> depth_attachments;
 
-      const framebuffer_ops& self;
+    const framebuffer_ops& self;
 
-      fbo2d(const framebuffer_ops& s) 
-        : self(s) {}
+    fbo2d(const framebuffer_ops& s) 
+      : self(s) {}
 
-      ~fbo2d() {
-        // TODO:
-      }
+    ~fbo2d() {
+      // TODO:
+    }
 
-      auto make_fbo(uint32_t width, uint32_t height) {
-        GLuint fbo = 0;
-        GL_FN(glGenFramebuffers(1, &fbo));
-        GL_FN(glBindFramebuffer(GL_FRAMEBUFFER, fbo));
+    auto make_fbo(uint32_t width, uint32_t height) {
+      GLuint fbo = 0;
+      GL_FN(glGenFramebuffers(1, &fbo));
+      GL_FN(glBindFramebuffer(GL_FRAMEBUFFER, fbo));
 
-        auto depth_attachment = g_m.textures->new_texture(g_m.textures->depthtexture_params(width, height));
-        auto color_attachment = g_m.textures->new_texture(g_m.textures->texture2d_rgba_params(width, height));
+      auto depth_attachment = g_m.textures->new_texture(g_m.textures->depthtexture_params(width, height));
+      auto color_attachment = g_m.textures->new_texture(g_m.textures->texture2d_rgba_params(width, height));
 
-        GL_FN(glFramebufferTexture2D(GL_FRAMEBUFFER, 
-                                     GL_COLOR_ATTACHMENT0, 
-                                     GL_TEXTURE_2D, 
-                                     g_m.textures->handle(color_attachment), 
-                                     0));
+      GL_FN(glFramebufferTexture2D(GL_FRAMEBUFFER, 
+                                    GL_COLOR_ATTACHMENT0, 
+                                    GL_TEXTURE_2D, 
+                                    g_m.textures->handle(color_attachment), 
+                                    0));
 
-        GL_FN(glFramebufferTexture2D(GL_FRAMEBUFFER, 
-                                     GL_DEPTH_ATTACHMENT, 
-                                     GL_TEXTURE_2D, 
-                                     g_m.textures->handle(depth_attachment), 
-                                     0));
+      GL_FN(glFramebufferTexture2D(GL_FRAMEBUFFER, 
+                                    GL_DEPTH_ATTACHMENT, 
+                                    GL_TEXTURE_2D, 
+                                    g_m.textures->handle(depth_attachment), 
+                                    0));
 
-        GL_FN(glBindFramebuffer(GL_FRAMEBUFFER, 0));
+      GL_FN(glBindFramebuffer(GL_FRAMEBUFFER, 0));
 
-        index_type new_handle = fbos.size();
+      index_type new_handle = fbos.size();
+      
+      fbos.push_back(fbo);
+      widths.push_back(width);
+      heights.push_back(height);
+      color_attachments.push_back(color_attachment);
+      depth_attachments.push_back(depth_attachment);
+
+      return new_handle;
+    }
+
+    void bind(index_type handle) const {
+      GL_FN(glBindFramebuffer(GL_FRAMEBUFFER, fbos[handle]));
+      GL_FN(glViewport(0, 0, widths[handle], heights[handle]));
+    }
+
+    void unbind(index_type handle) const {
+      GL_FN(glBindFramebuffer(GL_FRAMEBUFFER, 0));
+
+      ASSERT_CODE(
+        GLint viewport[4] = { 0 }; 
+        GL_FN(glGetIntegerv(GL_VIEWPORT, &viewport[0]));
         
-        fbos.push_back(fbo);
-        widths.push_back(width);
-        heights.push_back(height);
-        color_attachments.push_back(color_attachment);
-        depth_attachments.push_back(depth_attachment);
+        ASSERT(viewport[0] == 0);
+        ASSERT(viewport[1] == 0);
+        ASSERT(viewport[2] == widths[handle]);
+        ASSERT(viewport[3] == heights[handle]);
+      );
 
-        return new_handle;
-      }
-
-      void bind(index_type handle) const {
-        GL_FN(glBindFramebuffer(GL_FRAMEBUFFER, fbos[handle]));
-        GL_FN(glViewport(0, 0, widths[handle], heights[handle]));
-      }
-
-      void unbind(index_type handle) const {
-        GL_FN(glBindFramebuffer(GL_FRAMEBUFFER, 0));
-
-        ASSERT_CODE(
-          GLint viewport[4] = { 0 }; 
-          GL_FN(glGetIntegerv(GL_VIEWPORT, &viewport[0]));
-          
-          ASSERT(viewport[0] == 0);
-          ASSERT(viewport[1] == 0);
-          ASSERT(viewport[2] == widths[handle]);
-          ASSERT(viewport[3] == heights[handle]);
-        );
-
-        GL_FN(glViewport(0, 0, self.width, self.height));
-      }
-    };
+      GL_FN(glViewport(0, 0, self.width, self.height));
+    }
+  };
 
   struct render_cube {
     std::vector<module_textures::index_type> tex_color_handles;
@@ -114,152 +114,150 @@ struct framebuffer_ops {
         neg_z
     };
 
-  render_cube(const framebuffer_ops& f) : self(f),
-    cwidth(256),
-    cheight(256){
-  }
+    render_cube(const framebuffer_ops& f) : self(f),
+      cwidth(256),
+      cheight(256){
+    }
 
-        auto calc_look_at_mats(const vec3_t& position, real_t radius) {
-            real_t offset{R(1.0)};
-            
-            face_mats_type m;
+    auto calc_look_at_mats(const vec3_t& position, real_t radius) {
+      real_t offset{R(1.0)};
+      
+      face_mats_type m;
 
-            m[pos_x] = glm::lookAt(position + SPHERE_RIGHT(radius),
-                                       position + SPHERE_RIGHT(radius + offset),
-                                       V3_UP);
-            
-            m[neg_x] = glm::lookAt(position + SPHERE_LEFT(radius),
-                                   position + SPHERE_LEFT(radius + offset),
-                                   V3_UP);
+      m[pos_x] = glm::lookAt(position + SPHERE_RIGHT(radius),
+                                  position + SPHERE_RIGHT(radius + offset),
+                                  V3_UP);
+      
+      m[neg_x] = glm::lookAt(position + SPHERE_LEFT(radius),
+                              position + SPHERE_LEFT(radius + offset),
+                              V3_UP);
 
-            m[pos_y] = glm::lookAt(position + SPHERE_UP(radius),
-                                   position + SPHERE_UP(radius + R(5.0)),
-                                   V3_BACKWARD);
+      m[pos_y] = glm::lookAt(position + SPHERE_UP(radius),
+                              position + SPHERE_UP(radius + R(5.0)),
+                              V3_BACKWARD);
 
-            m[neg_y] = glm::lookAt(position + SPHERE_DOWN(radius),
-                                   position + SPHERE_DOWN(radius + offset),
-                                   V3_FORWARD);
+      m[neg_y] = glm::lookAt(position + SPHERE_DOWN(radius),
+                              position + SPHERE_DOWN(radius + offset),
+                              V3_FORWARD);
 
-            m[pos_z] = glm::lookAt(position + SPHERE_BACKWARD(radius),
-                                   position + SPHERE_BACKWARD(radius + offset),
-                                   V3_UP);
+      m[pos_z] = glm::lookAt(position + SPHERE_BACKWARD(radius),
+                              position + SPHERE_BACKWARD(radius + offset),
+                              V3_UP);
 
-            m[neg_z] = glm::lookAt(position + SPHERE_FORWARD(radius),
-                                   position + SPHERE_FORWARD(radius + offset),
-                                   V3_UP);
+      m[neg_z] = glm::lookAt(position + SPHERE_FORWARD(radius),
+                              position + SPHERE_FORWARD(radius + offset),
+                              V3_UP);
 
-            return m;
-
-        }
+      return m;
+    }
         
-        auto add(const vec3_t& position, real_t radius) {           
-            auto rcubeid = tex_color_handles.size();
-	    
-            tex_color_handles.push_back(g_m.textures->new_texture(g_m.textures->cubemap_params(cwidth, cheight)));
+    auto add(const vec3_t& position, real_t radius) {           
+      auto rcubeid = tex_color_handles.size();
+
+      tex_color_handles.push_back(g_m.textures->new_texture(g_m.textures->cubemap_params(cwidth, cheight)));
 
 #if defined(ENVMAP_CUBE_DEPTH)
-            tex_depth_handles.push_back(g_m.textures->new_cubemap(cwidth,
-                                                               cheight,
-                                                               GL_DEPTH_COMPONENT));
+      tex_depth_handles.push_back(g_m.textures->new_cubemap(cwidth,
+                                                          cheight,
+                                                          GL_DEPTH_COMPONENT));
 #else
-	    tex_depth_handles.push_back(g_m.textures->new_texture(g_m.textures->depthtexture_params(cwidth, cheight)));
+      tex_depth_handles.push_back(g_m.textures->new_texture(g_m.textures->depthtexture_params(cwidth, cheight)));
 #endif // ENVMAP_CUBE_DEPTH
-	    
-            positions.push_back(position);
-            
-            faces.push_back(calc_look_at_mats(position, radius));
 
-            {
-                GLuint fbo = 0;
-                GL_FN(glGenFramebuffers(1, &fbo));
-                fbos.push_back(fbo);
-            }
+      positions.push_back(position);
+      
+      faces.push_back(calc_look_at_mats(position, radius));
 
-            return static_cast<index_type>(rcubeid);
-        }
+      {
+        GLuint fbo = 0;
+        GL_FN(glGenFramebuffers(1, &fbo));
+        fbos.push_back(fbo);
+      }
 
-        void bind(index_type cube_id) const {
-          GL_FN(glBindFramebuffer(GL_FRAMEBUFFER, 
-                                  fbos.at(cube_id)));
+      return static_cast<index_type>(rcubeid);
+    }
 
-          GL_FN(glViewport( 0,
-                            0,
-                            cwidth,
-                            cheight));
-        }
+    void bind(index_type cube_id) const {
+      GL_FN(glBindFramebuffer(GL_FRAMEBUFFER, 
+                              fbos.at(cube_id)));
 
-        std::vector<uint8_t> get_pixels(index_type cube_id) const {
-            auto sz = cwidth * cheight * 4 * 6;
-            std::vector<uint8_t> all_faces(static_cast<size_t>(sz), 0x7f);
+      GL_FN(glViewport( 0,
+                        0,
+                        cwidth,
+                        cheight));
+    }
 
-            GL_FN(glGetTextureImage(g_m.textures->handle(tex_color_handles.at(cube_id)),
-                                    0,
-                                    GL_RGBA,
-                                    GL_UNSIGNED_BYTE,
-                                    static_cast<GLsizei>(all_faces.size()),
-                                    &all_faces[0]));
+    std::vector<uint8_t> get_pixels(index_type cube_id) const {
+      auto sz = cwidth * cheight * 4 * 6;
+      std::vector<uint8_t> all_faces(static_cast<size_t>(sz), 0x7f);
 
-            return std::move(all_faces);
-        }
+      GL_FN(glGetTextureImage(g_m.textures->handle(tex_color_handles.at(cube_id)),
+                              0,
+                              GL_RGBA,
+                              GL_UNSIGNED_BYTE,
+                              static_cast<GLsizei>(all_faces.size()),
+                              &all_faces[0]));
+
+      return std::move(all_faces);
+    }
         
-        glm::mat4 set_face(index_type cube_id, axis face) const {
-            GL_FN(glFramebufferTexture2D(GL_FRAMEBUFFER,
-                                         GL_COLOR_ATTACHMENT0,
-                                         GL_TEXTURE_CUBE_MAP_POSITIVE_X + static_cast<GLenum>(face),
-                                         g_m.textures->handle(tex_color_handles.at(cube_id)),
-                                         0));
+    glm::mat4 set_face(index_type cube_id, axis face) const {
+      GL_FN(glFramebufferTexture2D(GL_FRAMEBUFFER,
+                                    GL_COLOR_ATTACHMENT0,
+                                    GL_TEXTURE_CUBE_MAP_POSITIVE_X + static_cast<GLenum>(face),
+                                    g_m.textures->handle(tex_color_handles.at(cube_id)),
+                                    0));
 #ifdef ENVMAP_CUBE_DEPTH
-	    auto target = GL_TEXTURE_CUBE_MAP_POSITIVE_X + static_cast<GLenum>(face);
+      auto target = GL_TEXTURE_CUBE_MAP_POSITIVE_X + static_cast<GLenum>(face);
 #else
-	    auto target = GL_TEXTURE_2D;
+      auto target = GL_TEXTURE_2D;
 #endif
-	    
-            GL_FN(glFramebufferTexture2D(GL_FRAMEBUFFER,
-                                         GL_DEPTH_ATTACHMENT,
-                                         target,
-                                         g_m.textures->handle(tex_depth_handles.at(cube_id)),
-                                         0));
+  
+      GL_FN(glFramebufferTexture2D(GL_FRAMEBUFFER,
+                                    GL_DEPTH_ATTACHMENT,
+                                    target,
+                                    g_m.textures->handle(tex_depth_handles.at(cube_id)),
+                                    0));
 
-            auto fbcheck = glCheckFramebufferStatus(GL_FRAMEBUFFER);
-            if (fbcheck != GL_FRAMEBUFFER_COMPLETE) {
-                write_logf("FRAMEBUFFER BIND ERROR: code returned = 0x%x", fbcheck);
-            }
-            ASSERT(fbcheck == GL_FRAMEBUFFER_COMPLETE);
+      auto fbcheck = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+      if (fbcheck != GL_FRAMEBUFFER_COMPLETE) {
+        write_logf("FRAMEBUFFER BIND ERROR: code returned = 0x%x", fbcheck);
+      }
+      ASSERT(fbcheck == GL_FRAMEBUFFER_COMPLETE);
 
-            const auto& viewmats = faces.at(cube_id);
-            
-            return viewmats.at(face);
-        }
-
-        void unbind() {
-            GL_FN(glBindFramebuffer(GL_FRAMEBUFFER, 0));
-	        GL_FN(glViewport(0, 0, self.width, self.height));
-        }
-    };
-    
-
-    std::unique_ptr<render_cube> rcube;
-    std::unique_ptr<fbo2d> fbos;
-    
-    framebuffer_ops(uint32_t w, uint32_t h)
-        :   count(0),
-            width(w),
-        height(h),
-        rcube{new render_cube(*this)},
-        fbos{new fbo2d(*this)}
-    {}
-    
-    void screenshot();
-
-    auto add_render_cube(const vec3_t& position, real_t radius) {
-        return rcube->add(position, radius);
+      const auto& viewmats = faces.at(cube_id);
+      
+      return viewmats.at(face);
     }
 
-    auto add_fbo(uint32_t w, uint32_t h) {
-      return fbos->make_fbo(w, h);
+    void unbind() {
+      GL_FN(glBindFramebuffer(GL_FRAMEBUFFER, 0));
+      GL_FN(glViewport(0, 0, self.width, self.height));
     }
+  };
+    
+  std::unique_ptr<render_cube> rcube;
+  std::unique_ptr<fbo2d> fbos;
+  
+  framebuffer_ops(uint32_t w, uint32_t h)
+    : count(0),
+      width(w),
+      height(h),
+      rcube{new render_cube(*this)},
+      fbos{new fbo2d(*this)}
+  {}
+  
+  void screenshot();
 
-    auto render_cube_color_tex(index_type r) const {
-        return rcube->tex_color_handles.at(r);
-    }
+  auto add_render_cube(const vec3_t& position, real_t radius) {
+    return rcube->add(position, radius);
+  }
+
+  auto add_fbo(uint32_t w, uint32_t h) {
+    return fbos->make_fbo(w, h);
+  }
+
+  auto render_cube_color_tex(index_type r) const {
+    return rcube->tex_color_handles.at(r);
+  }
 };
