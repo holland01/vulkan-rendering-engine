117b5fd0a9bbc0915143a071ac6399f6e2c8329e
schutth <schutth@wwu.edu>
2019-11-19 20:03:02 -0800

    BASE_DEBUG: general purpose debug source macro
    Eventually, all debug features should be dependent on whether or not
    this is turned on. Included is a usecase example with g_log_mask in
    util.cpp

--- Makefile
+++ Makefile
@@ -24,7 +24,7 @@ CXXFLAGS=-std=c++17 -Wall -Wpedantic -Werror -Wno-unused-function
 CFLAGS=
 
 ifdef DEBUG
-	CXXFLAGS += -g -ggdb -O0
+	CXXFLAGS += -g -ggdb -O0 -DBASE_DEBUG=1
 	CFLAGS += -g -ggdb -O0
 else
 	CXXFLAGS += -O2
--- base/util.cpp
+++ base/util.cpp
@@ -1,10 +1,14 @@
 #include "util.hpp"
 
+#if defined(BASE_DEBUG)
 unsigned long long g_log_mask = 
   logflag_programs_load | 
   logflag_programs_use_program | 
   logflag_textures_bind | 
   logflag_render_pipeline_pass_info_apply;
+#else 
+unsigned long long g_log_mask = 0;
+#endif
 
 static std::vector<std::string> g_gl_err_msg_cache;
 
