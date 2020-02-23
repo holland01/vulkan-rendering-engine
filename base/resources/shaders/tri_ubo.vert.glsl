23b283b4d99856872e080b3363fdf18383a972d9
schutth <schutth@wwu.edu>
2020-02-22 19:41:58 -0800

    rewrite method for fixing gl_Position

--- base/resources/shaders/tri_ubo.vert.glsl
+++ base/resources/shaders/tri_ubo.vert.glsl
@@ -12,12 +12,14 @@ layout(set = 1, binding = 0) uniform transforms_uniform_block {
   mat4 worldToView;
 };
 
+vec4 fixup_position(in vec4 p) {
+  p.y = -p.y; // invert y axis since vulkan's coordinate system is inverted on Y
+  p.z = (p.z + p.w) * 0.5; // map NDC [-1,1] to NDC [0, 1]
+  return p;
+}
 
 void main() {  
-  gl_Position = viewToClip * worldToView * vec4(in_Position, 1.0);
-  gl_Position.y = -gl_Position.y; // invert y axis since vulkan's coordinate system is inverted on Y
-  gl_Position.z = (gl_Position.z + gl_Position.w) / 2.0; // map NDC [-1,1] to NDC [0, 1]
-
+  gl_Position = fixup_position(viewToClip * worldToView * vec4(in_Position, 1.0));
   frag_TexCoord = in_TexCoord;
   frag_Color = in_Color;
 }
